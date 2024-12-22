97849cecbeb3a5f1b17c4d7bb2e42de1e3eef102
Hugo Guay <dancinoman@gmail.com>
2024-12-22 15:28:16 -0500

    Refactored and added log

--- data/bot.py
+++ data/bot.py
@@ -3,6 +3,7 @@ import pandas
 import csv
 import time
 import datetime
+import traceback
 
 # Selenium imports
 from selenium import webdriver
@@ -15,71 +16,134 @@ from selenium.webdriver.support import expected_conditions as ec
 import chromedriver_binary
 
 from bs4 import BeautifulSoup
+import bs4
 
 class GatherData:
 
+    driver = webdriver.Chrome()
     data_source = "https://www.restomontreal.ca/s/?restaurants=greater-montreal&lang=en"
+    date = datetime.date.today().strftime('%d-%m-%Y')
+    hour = datetime.datetime.now().strftime('%H:%M:%S')
+    all_log_txt = []
     start_time = time.time()
 
-    def display_progress(self, message: str):
+    def create_log(self, status: str, value: str):
 
-        time_seconds = round(time.time() - self.start_time)
-        time_format = str(datetime.timedelta(seconds=time_seconds))
-        print(f"({time_format}) {message}")
+        # Create readable log
+        log_info = f"[{self.date} {self.hour}] {status}: {value}"
+        print(log_info)
+        # Prepare text to save in file
+        self.all_log_txt.append(log_info)
 
-    def create_log(self, name: str, value: str):
-        print("log: ", name, value)
+    def initialize_gathering(self, page_link: str, *args):
 
-    def initialize_gathering(self, page_link: str, page_range: list):
+        try:
+            # Display progression on terminal and write in log
+            self.create_log('INFO', 'Page is loading...')
+            # Initializing webdriver
+            self.driver.get(page_link)
+            time.sleep(3)
 
-        # Display progression on terminal
-        self.display_progress('Page is loading...')
-        # Initializing webdriver
-        driver = webdriver.Chrome()
-        driver.get(page_link)
-        time.sleep(3)
+            # Initiate soup
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            # Track number of results
+            num_result_block = soup.find("a", id="tab-restaurants-active")
+            num_restults = num_result_block.find("span").text.replace("(", "").replace(")","")
 
-        #Using beautiful soup to get the list of restaurants
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        resto_block = soup.find_all("div", class_='search-result')
+            # Track number of pages
+            num_pages = soup.find("div", class_="mb0 mt40 color-dark bold fs-16 text-center p10").text.split()[-1].strip()
+            # Saving infon into log
+            self.create_log('INFO', 'Started')
+            self.create_log('INFO' , f'Number of result(s) for scraping {num_restults}')
+            self.create_log('INFO', f'Number of page(s) for scraping {num_pages}')
 
-        #Track number of results
-        num_result_block = soup.find("a", id="tab-restaurants-active")
-        num_restults = num_result_block.find("span").text.replace("(", "").replace(")","")
+            # Process args for instructions
+            for item in args:
+                if isinstance(item, str):
+                    if item == "all":
+                        self.get_data(1, int(num_pages))
 
-        self.display_progress(f'Today {num_restults} are available')
-        # Saving infon into log
-        self.create_log('Scraping date: ', datetime.date.today().strftime('%d %B, %Y'))
-        self.create_log('Scraping time: ', datetime.datetime.now().strftime('%H:%M:%S'))
-        self.create_log('Number of results: ', num_restults)
-        self.display_progress('Info located ready to fetch...')
 
-        restaurants = []
+                if isinstance(item, (list, tuple)):
+                    if len(item) == 1:
+                        self.get_data(1, item[0])
+                    elif len(item) == 2:
+                        self.get_data(item[0], item[1])
+                    else:
+                        self.get_data(0, 0, item)
+        except Exception:
+            full_trace = traceback.format_exc()
+            self.create_log('ERROR', full_trace)
+            self.create_log('WARNING', 'Scraping process interupted no record was made.')
 
-        for i, resto in enumerate(resto_block):
 
-            # From main Div
-            lat = resto.get('data-lat')
-            lon = resto.get('data-lon')
+    def get_data(self, min_page: int, max_page: int, pages = None):
 
-            # From link
-            link = resto.find_all('a')
-            resto_link = link[2]
-            perso_page = resto_link.get('href')
-            name = resto_link.text.strip()
 
-            # From span
-            address = resto.find('span', id=f'{i}-searchaddress').text.strip()
+        # Scrape by page
+        try:
+            for page_num in range(min_page, max_page + 1):
 
-            # From hash tag
-            hash_div = resto.find('div', class_='color-gray mb5 lh-normal search-cuisine-box')
-            hash_elements = hash_div.find_all('a')
+                # Start scraping content to the instruction page
+                web_link = self.data_source + f"&page={page_num}#{page_num}"
+                self.driver.get(web_link)
+                time.sleep(3)
 
-            hash_tags = []
-            for hash in hash_elements:
-                hash_tags.append(hash.text.strip())
-            if i == 2:
+                #Using beautiful soup to get the list of restaurants
+                soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+                resto_block = soup.find_all("div", class_='search-result')
+
+                self.create_log('INFO', 'Starting to fetch...')
+                restaurants = []
+
+                # Scrape all content block
+                for i, resto in enumerate(resto_block, start=1):
+
+                    # From main Div
+                    lat = resto.get('data-lat')
+                    lon = resto.get('data-lon')
+
+                    # From link
+                    link = resto.find_all('a')
+                    resto_link = link[2]
+                    unique_page = resto_link.get('href')
+                    name = resto_link.text.strip()
+
+                    # From span
+                    address = resto.find('span', id=f'{i - 1}-searchaddress').text.strip()
+
+                    # From hash tag
+                    hash_div = resto.find('div', class_='color-gray mb5 lh-normal search-cuisine-box')
+                    hash_elements = hash_div.find_all('a')
+
+                    hash_tags = []
+                    for hash in hash_elements:
+                        hash_tags.append(hash.text.strip())
+
+                    restaurants.append(
+                        {
+                            "id": i,
+                            "name": name,
+                            "latitude": lat,
+                            "longitude": lon,
+                            "address": address,
+                            "hash_tags": ",".join(hash_tags),
+                            "unique_page": unique_page
+                        }
+                    )
+
+                print(f'Page {page_num} is done')
+                print(len(restaurants))
+
+                #TODO remove line below after test
                 break
+                # Stop if reached the last page
+                if page_num == max_page:
+                    break
+        except Exception:
+            full_trace = traceback.format_exc()
+            self.create_log('ERROR', full_trace)
+            self.create_log('WARNING', f'Process were interrupted at page {page_num} after {i - 1} element(s) recored')
 
         def run_next_page(self):
             pass
@@ -104,7 +168,6 @@ class GatherData:
             #    'max peak'     : peak_online
             #})
 
-        print(sorted(hash_tags))
         #Then change page
         #resto_list = soup.find_all('tr', class_='app')
         # Interact with webpage
@@ -129,4 +192,8 @@ class GatherData:
 gathering = GatherData()
 
 # Scrape the page
-gathering.initialize_gathering(gathering.data_source, [1])
+gathering.initialize_gathering(gathering.data_source, "all")
+
+# Create a log file
+with open(f"logs/bot_log_{gathering.date}_{gathering.hour}.log", "a") as log_file:
+    log_file.write("\n".join(gathering.all_log_txt))
