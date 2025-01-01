238a0e6a158640d47667f723ddc62789ce14205e
Hugo Guay <dancinoman@gmail.com>
2025-01-01 14:14:34 -0500

    started unique page

--- bot.py
+++ bot.py
@@ -27,6 +27,9 @@ class GatherData:
     date = datetime.date.today().strftime('%d-%m-%Y')
     hour = datetime.datetime.now().strftime('%H:%M:%S')
     all_log_txt = []
+    restaurants = []
+    detailed_restaurants = []
+    rating_restaurants = []
     start_time = time.time()
 
     def create_log(self, status: str, value: str):
@@ -84,6 +87,10 @@ class GatherData:
                         self.get_data(item[0], item[1] + 1)
                     else:
                         self.get_data(0, 0, item)
+
+                if isinstance(item, int):
+                    self.get_data(0 , 0, [item])
+
         except Exception:
             full_trace = traceback.format_exc()
             self.create_log('ERROR', full_trace)
@@ -94,135 +101,137 @@ class GatherData:
 
 
         # Scrape by page
-        try:
+
+
+            def execute_scrape(page_num, id):
+
+                try:
+                    # Start scraping content to the instruction page
+                    web_link = self.data_source + f"&page={page_num}#{page_num}"
+                    self.driver.get(web_link)
+                    time.sleep(3)
+
+                    #Using beautiful soup to get the list of restaurants
+                    soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+                    resto_block = soup.find_all("div", class_='search-result')
+
+                    self.create_log('INFO', f'Starting to fetch on page {page_num}')
+
+                    # Scrape all content block
+                    for i, resto in enumerate(resto_block):
+
+                        # From main Div
+                        lat = resto.get('data-lat')
+                        lon = resto.get('data-lon')
+
+                        # From link
+                        link = resto.find_all('a')
+                        resto_link = link[2]
+                        unique_page = resto_link.get('href')
+                        raw_name = resto_link.text.split()
+                        name = " ".join(raw_name)
+
+                        # From span
+                        address = resto.find('span', id=f'{i}-searchaddress').text.strip()
+
+                        # From hash tag
+                        hash_div = resto.find('div', class_='color-gray mb5 lh-normal search-cuisine-box')
+                        # Skip if there is no hash tag
+                        if hash_div != None:
+                            hash_elements = hash_div.find_all('a')
+
+                            hash_tags = []
+                            for hash in hash_elements:
+                                hash_tags.append(hash.text.strip())
+
+                        self.restaurants.append(
+                            {
+                                "id": id,
+                                "name": name,
+                                "latitude": lat,
+                                "longitude": lon,
+                                "address": address,
+                                "hash_tags": ",".join(hash_tags),
+                                "unique_page": unique_page
+                            }
+                        )
+
+                        # Get details of in unique page for restaurant
+                        self.get_detailed_page(unique_page, id)
+
+                        id += 1
+
+                        self.create_log('INFO', f'{len(self.restaurants)} - data')
+
+
+                        # Stop if reached the last page
+                        if page_num == max_page+ 1:
+                            self.create_log('INFO', "Bot's task completed")
+                            break
+                except Exception:
+                    full_trace = traceback.format_exc()
+                    self.create_log('ERROR', full_trace)
+                    self.create_log('WARNING', f'Process were interrupted at page {page_num} after {i} element(s) recored')
+
+            # Execute a range or list of page
             id = 1
-            restaurants = []
 
-            for page_num in range(min_page, max_page):
+            if max_page != 0:
 
-                # Start scraping content to the instruction page
-                web_link = self.data_source + f"&page={page_num}#{page_num}"
-                self.driver.get(web_link)
-                time.sleep(3)
+                for page_num in range(min_page, max_page):
+                    execute_scrape(page_num, id)
 
-                #Using beautiful soup to get the list of restaurants
-                soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-                resto_block = soup.find_all("div", class_='search-result')
-
-                self.create_log('INFO', f'Starting to fetch on page {page_num}')
-
-                # Scrape all content block
-                for i, resto in enumerate(resto_block):
-
-                    # From main Div
-                    lat = resto.get('data-lat')
-                    lon = resto.get('data-lon')
-
-                    # From link
-                    link = resto.find_all('a')
-                    resto_link = link[2]
-                    unique_page = resto_link.get('href')
-                    raw_name = resto_link.text.split()
-                    name = " ".join(raw_name)
-
-                    # From span
-                    address = resto.find('span', id=f'{i}-searchaddress').text.strip()
-
-                    # From hash tag
-                    hash_div = resto.find('div', class_='color-gray mb5 lh-normal search-cuisine-box')
-                    # Skip if there is no hash tag
-                    if hash_div != None:
-                        hash_elements = hash_div.find_all('a')
-
-                        hash_tags = []
-                        for hash in hash_elements:
-                            hash_tags.append(hash.text.strip())
-
-                    restaurants.append(
-                        {
-                            "id": id,
-                            "name": name,
-                            "latitude": lat,
-                            "longitude": lon,
-                            "address": address,
-                            "hash_tags": ",".join(hash_tags),
-                            "unique_page": unique_page
-                        }
-                    )
-
-                    id += 1
-
-                self.create_log('INFO', f'Amount of data in memory is {len(restaurants)} restaurants')
-
-
-                # Stop if reached the last page
-                if page_num == max_page+ 1:
-                    self.create_log('INFO', "Bot's task completed")
-                    break
+            elif pages is not None:
+                for page in pages:
+                    execute_scrape(page, id)
 
             # Save into csv file
-            self.df_resto = pd.DataFrame(restaurants)
-            self.df_resto.to_csv('data/resto-list/restaurant_montreal.csv', index=False)
-
-            # Starting process of unique page
-            self.get_detailed_page()
-
-        except Exception:
-            full_trace = traceback.format_exc()
-            self.create_log('ERROR', full_trace)
-            self.create_log('WARNING', f'Process were interrupted at page {page_num} after {i - 1} element(s) recored')
-
-    def get_detailed_page(self):
-
-        # Go to each personal restaurant's link
-        for personal_page_link in self.df_resto['unique_page']:
-
-            self.driver.get(personal_page_link)
-            time.sleep(6)
+                self.df_resto = pd.DataFrame(self.restaurants)
+                self.df_resto.to_csv('data/resto-list/restaurant_montreal.csv', index=False)
 
 
-        def run_next_page(self):
-            pass
-            #all elements example
-            #name = game.find('a', class_='b').text.strip()
 
-            # Get another list inside of the element of list
-            #elements = game.find_all('td', class_ = 'dt-type-numeric')
+    def get_detailed_page(self, link, id_key):
 
-            #price = elements[2].text.replace('CDN$', '').strip()
-            #rating = elements[3].text.replace('%', '').strip()
-            #release_date = elements[4].text.strip()
-            #followers = elements[5].text.replace(',', '').strip()
-            #peak_online = elements[7].text.replace(',', '').strip()
+        self.driver.get(link)
+        time.sleep(3)
+        # Get page soup
+        detailed_soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 
-            #games.append({
-            #    'name'         : name,
-            #    'price'        : price,
-            #    'rating'       : rating,
-            #    'release date' : release_date,
-            #    'followers'    : followers,
-            #    'max peak'     : peak_online
-            #})
+        amount_of_picture  = detailed_soup.find('a', class_='btn-e-see-photos').text.strip()
+        average_price = detailed_soup.find('div', class_='restaurant-price').get('data-tooltip')
+        # Get features list
+        features_group = detailed_soup.find('div', class_="row mt5 mb5").find_all('span', class_='action-btn-group')
+        features = []
 
-        #Then change page
-        #resto_list = soup.find_all('tr', class_='app')
-        # Interact with webpage
-        #wait = WebDriverWait(driver, 15).until(lambda x : x.find_element(By.ID, 'dt-length-0'))
-        #get_input = Select(driver.find_element(By.ID, 'dt-length-0'))
-        #get_input.select_by_visible_text('All (slow)')
-        #wait = WebDriverWait(driver, 5)
-        #wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='main']")))
-
-        #for  in games_list:
+        for feature in features_group:
+            features.append(feature.find('span').text.strip())
 
 
-        #print(f' Writing {len(games)} element(s)')
-        #with open(f'data/csv/games_top_{year}.csv', 'w') as file:
-        #    writer = csv.DictWriter(file, fieldnames= games[0].keys())
-        #    writer.writeheader()
-        #    writer.writerows(games)
-        #print('1 page done successfully')
-        #driver.quit()
+        description = detailed_soup.find('div', class_='aboutus-text').text.strip()
+        print(description)
+        affiliated = detailed_soup.find('h3', class_='title-section').text.strip()
+        #print(affiliated)
+
+        average_rating = detailed_soup.find('span', class_='google_rating_bold').text.strip()
+        amount_of_rating = detailed_soup.find('a', class_='reviewscard_rating').text.strip()
+        print(average_rating)
+
+        self.detailed_restaurants.append(
+            {
+                "id_resto": id_key,
+                "amount_pictures": amount_of_picture,
+                "average_price": average_price,
+                "features": ",".join(features)
+            }
+        )
+
+        self.rating_restaurants.append(
+            {
+                "id_resto": id_key
+            }
+        )
+
 
 
 #Initialize
