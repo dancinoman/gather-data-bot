112b7404095c97373692a431e5f6675c6401957f
Hugo Guay <dancinoman@gmail.com>
2025-04-18 15:06:21 -0400

    refactored classes roles and docstring

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+
+# Basic imports
 import datetime
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # import classes
 from app.processor import Processor
+
 #Initialize
 date = datetime.date.today().strftime('%d-%m-%Y')
 folder_location = f'data/resto-list/date_{date}'
 website_address = os.environ.get('WEBSITE_ADDRESS')
 processor = Processor(website_address, folder_location)
 
-# Scrape the page
+# Initiate the method of gathering the data
 processor.initialize_gathering("all")
